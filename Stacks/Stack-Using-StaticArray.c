85a444b97c6d65f6dc964a098db4f9ab48554117
orbitze <mverma1427@gmail.com>
2021-08-31 01:09:24 +0530

    Update Stack-Using-StaticArray.c

--- Stacks/Stack-Using-StaticArray.c
+++ Stacks/Stack-Using-StaticArray.c
@@ -8,45 +8,38 @@ typedef struct stack {
     int *array;
     int capacity;
     int top;
+    int size;
 } StaticArrStack;
 
-StaticArrStack *CreateStack() {
-    StaticArrStack *S = malloc(sizeof(StaticArrStack));
-    if (!S)
-        return NULL;
-    S->capacity = MAXSIZE;
-    S->top = -1;
-    S->array = malloc(S->capacity * sizeof(int));
-    if (!S->array)
-        return NULL;
-    return S;
-}
 
+StaticArrStack *CreateStack();
+int GetSize(StaticArrStack *);
+int IsEmptyStack(StaticArrStack *);
 void Push(StaticArrStack *, int);
 int Pop(StaticArrStack *);
 int Top(StaticArrStack *);
-int GetSize(StaticArrStack *);
-int IsEmptyStack(StaticArrStack *);
+void DisplayStack(StaticArrStack *);
 void DeleteStack(StaticArrStack *);
-void DisplayStackElements(StaticArrStack *);
 
 
 int main() {
 
-    int elem, ch = -1;
+    int elem, ch = -1, popped;
     StaticArrStack *myStack = CreateStack();
 
     printf ("\n Current Status of the Stack: Empty\n\n");
+    
+    printf("\t======================\n");
+    printf("\t STACK-OPERATIONS MENU \n");
+    printf("\t======================\n");
+    printf("\n\t1 Push");
+    printf("\n\t2 Pop");
+    printf("\n\t3 Get Top");
+    printf("\n\t4 Display Stack");
+    printf("\n\t5 Delete Stack");
+    printf("\n\t0 EXIT\n");
+
     do {
-        printf("\t======================\n");
-        printf("\t STACK-OPERATIONS MENU \n");
-        printf("\t======================\n");
-        printf("\n\t1 Push element");
-        printf("\n\t2 Pop the stack");
-        printf("\n\t3 Get the Top");
-        printf("\n\t4 Display all the elements");
-        printf("\n\t5 Delete the Stack");
-        printf("\n\t0 EXIT\n");
         printf("\n\tEnter choice: ");
         scanf("%d", &ch);
 
@@ -57,20 +50,22 @@ int main() {
                 Push(myStack, elem);
                 break;
             case 2:
-                if (Pop(myStack) > INT_MIN)
-                    printf(" Popped element: %d\n", Pop(myStack));
+                popped = Pop(myStack);
+                if (popped > INT_MIN)
+                    printf(" Popped element: %d\n", popped);
                 break;
             case 3:
                 if (Top(myStack) > INT_MIN)
                     printf("\n\n Top of the stack: %d\n", Top(myStack));
                 break;
             case 4:
-                DisplayStackElements(myStack);
+                DisplayStack(myStack);
                 break;
             case 5:
                 DeleteStack(myStack);
                 break;
             case 0:
+                printf("\n GOODBYE!\n");
                 exit(1);
                 break;
             default:
@@ -84,9 +79,23 @@ int main() {
 }
 
 
+StaticArrStack *CreateStack() {
+    StaticArrStack *S = malloc(sizeof(StaticArrStack));
+    if (!S) {
+        printf("\nMemory error!\n");
+        return NULL;
+    }
+    S->capacity = MAXSIZE;
+    S->top = -1;
+    S->array = malloc(S->capacity * sizeof(int));
+    if (!S->array)
+        return NULL;
+    return S;
+}
 
 int GetSize(StaticArrStack *S) {
-    return (S->top == -1) ? 0 : (S->top+1);
+    // return (S->top == -1) ? 0 : (S->top+1);
+    return S->size;
 }
 
 
@@ -100,18 +109,9 @@ void Push(StaticArrStack *S, int val) {
         printf(" Stack Overflow! Can't push this element onto the stack!\n");
     else {
         S->array[++S->top] = val;
+        S->size++;
         printf(" %d has been pushed onto the stack!\n", val);
     }
-}
-
-
-int Top(StaticArrStack *S) {
-    if (IsEmptyStack(S)) {
-        printf("\n Can't get the top of the stack; Stack is empty!\n");
-        return INT_MIN;
-    }
-    else
-        return (S->array[S->top]);
 }
 
 
@@ -120,30 +120,45 @@ int Pop(StaticArrStack *S) {
         printf("\n Can't pop the stack; Stack is empty!\n");
         return INT_MIN;
     } else {
-        printf("\n Element has successfully been popped!\n");
+        printf("\n Element has been successfully popped!\n");
+        S->size--;
         return (S->array[S->top--]);
     }
 }
 
 
-void DeleteStack(StaticArrStack *S) {
-    if (S) {
-        if (S->array)
-            free(S->array);
-        free(S);
+int Top(StaticArrStack *S) {
+    if (IsEmptyStack(S)) {
+        printf("\n Can't get top of the stack; Stack is empty!\n");
+        return INT_MIN;
     }
-    printf("\n Stack elements have been successfully deleted!\n\n");
+    return (S->array[S->top]);
 }
 
 
-void DisplayStackElements(StaticArrStack *S) {
-    if (IsEmptyStack(S) == 1) {
+void DisplayStack(StaticArrStack *S) {
+    if (GetSize(S) == 0) {
         printf("\n\n Nothing there to display; Stack is empty!\n");
         return;
     } else {
-        printf("\n\n >> Current status of the stack:\n\n\t ---------\n");
+        printf("\n\n >> Current status of the Stack:\n\n\t ----------\n");
         for (int i = GetSize(S)-1; i >= 0; i--)
-            printf("\t |  %3d  |\n\t ---------\n", S->array[i]);
+            printf("\t |  %3d   |\n\t ----------\n", S->array[i]);
         printf("\n");
     }
+}
+
+
+void DeleteStack(StaticArrStack *S) {
+    if (S->size == 0) {
+        printf("\n Can't delete an empty stack!\n\n");
+        return;
+    }
+    else if (S) {
+        if (S->array)
+            free(S->array);
+        free(S);
+        S->size = 0;
+    }
+    printf("\n Stack has been successfully deleted!\n\n");
 }
